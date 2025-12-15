9a92a1db2c48eb4bd0443920819f3943aa1382ea
joosunghyun99 <joosunghyun99@gmail.com>
2025-12-15 10:38:31 +0900

    feat/JSH : 충돌 해결 시도

--- Assets/02_Scripts/04_SummonPrefab/LightningFairy.cs
+++ Assets/02_Scripts/04_SummonPrefab/LightningFairy.cs
@@ -1,9 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
-using static UnityEditor.Experimental.GraphView.GraphView;
-using static UnityEngine.GraphicsBuffer;
 
 public class LightningFairy : FairyBase
 {
